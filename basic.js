0f43c266bfc16b82e575cf161c6a070785bad89d
anandhere8 <anandhere8@gmail.com>
2019-12-27 12:26:03 +0530

    Refresh of jS

---
+++ basic.js
@@ -0,0 +1,16 @@
+var a = 4;
+var check = false;
+var str = "Anand" ;
+var f = 4.5;
+var d = 6.5585858;
+var arr=["Anand","Amitt","Akshay","Arjun",45];
+
+console.log(a);
+console.log(check);
+console.log(str);
+console.log(f);
+console.log(d);
+console.log(arr[3]);
+
+
+
