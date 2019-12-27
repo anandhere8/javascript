f81a2b3f97a2ac75c7e8c691c13cab7b09f7f2b5
anandhere8 <anandhere8@gmail.com>
2019-12-27 12:48:02 +0530

    comparator

---
+++ comparator.js
@@ -0,0 +1,17 @@
+if(4 == '4')
+    console.log("Equal in values");
+
+if(4 === '4')
+    console.log("And in type");
+
+if(4 != '4')
+    console.log("Yes");
+
+if(4 !== '4')
+    console.log("Not values");
+
+if(3 < 4)
+    console.log("3 is less than 4");
+    
+if(4 <= '4')
+    console.log("4 is equal in values");
