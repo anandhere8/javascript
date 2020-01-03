00e14614f0de8f03ff2d552997dfd99e003a9448
anandhere8 <anandhere8@gmail.com>
2020-01-04 02:42:06 +0530

    add note to localstorage

--- Notes/js/app.js
+++ Notes/js/app.js
@@ -1,18 +1,19 @@
 //console.log("Working");
 
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener('click',()=>{
-let addTxt = document.getElementById("addTxt");
-let notes = localStorage.getItem("notes");
-if(notes == null)
-{
-    noteobj = [];
-}
-else{
-        noteobj= JSON.parse(notes);
-}
+addBtn.addEventListener('click', () => {
+    let addTxt = document.getElementById("addTxt");
 
-noteobj.push(addTxt.value);
-console.log(noteobj);
+    let notes = localStorage.getItem("notes");
+    let noteobj =[];
+
+    if (notes != null)
+        noteobj = JSON.parse(notes);
+
+    noteobj.push(addTxt.value);
+   
+    
+    localStorage.setItem("notes",JSON.stringify(noteobj));  
+
 });
 
