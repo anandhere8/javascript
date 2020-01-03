34277af7407d71cc19fe5098a543472ab38f4394
anandhere8 <anandhere8@gmail.com>
2020-01-04 03:06:03 +0530

    Not showing yet

--- Notes/js/app.js
+++ Notes/js/app.js
@@ -5,15 +5,44 @@ addBtn.addEventListener('click', () => {
     let addTxt = document.getElementById("addTxt");
 
     let notes = localStorage.getItem("notes");
-    let noteobj =[];
+    let noteobj = [];
 
     if (notes != null)
         noteobj = JSON.parse(notes);
 
     noteobj.push(addTxt.value);
-   
-    
-    localStorage.setItem("notes",JSON.stringify(noteobj));  
+
+
+    localStorage.setItem("notes", JSON.stringify(noteobj));
+    showNotes();
 
 });
+
+var showNotes = () => {
+    let notes = localStorage.getItem("notes");
+    let html = "";
+    
+    if (notes == null)
+        noteobj = [];
+    else
+        noteobj = JSON.parse(notes);
+
+    noteobj.forEach(function (element, index) {
+   
+        html += `
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+
+            <div class="card-body">
+                <h5 class="card-title">${index+1} Note</h5>
+                <p class="card-text">${element} </p>
+                <button class="btn btn-primary">Delete Note</button>
+            </div>
+        </div>
+                `                
+});
+    let noteele = document.getElementById('notes');
+    if(notes.length == 0)
+    noteele.innerText = html;
+        
+}
 
